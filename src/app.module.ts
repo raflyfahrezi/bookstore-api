c2d948a83dfbcd059a2393195ff45e618c645fb2
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2024-10-29 00:34:17 +0700

    fix app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
+import 'dotenv/config';
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { BooksController, BooksModule, BooksService } from './resources';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [BooksModule],
+  controllers: [AppController, BooksController],
+  providers: [AppService, BooksService],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  default(): string {
+    return 'Welcome to Bookstore API';
   }
 }
